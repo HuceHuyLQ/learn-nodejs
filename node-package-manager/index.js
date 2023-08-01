cd9b9c352a81bcc88391432fc621be5290a47392
quagh03 <03.quanghuy.le@gmail.com>
2023-08-01 14:10:48 +0700

    practice

--- node-package-manager/index.js
+++ node-package-manager/index.js
@@ -1,6 +1,10 @@
 // var generateName = require("sillyname");
-
 import generateName from "sillyname";
+import superheroes from "superheroes";
 var sillyname = generateName();
 
 console.log(`Generated name is ${sillyname} !`);
+
+superheroes.all
+
+console.log(`I'm ${superheroes.random()}`);

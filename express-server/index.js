104eb58c03121425a6628d3ad1ddef9d2030dac2
quagh03 <03.quanghuy.le@gmail.com>
2023-08-01 15:08:34 +0700

    HTTP request

--- express-server/index.js
+++ express-server/index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/" , (req, res) => {
+    res.send("<h1>Hello</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<h1>About Me</h1><p>Quagh</p>");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>Contact Me</h1><p>Phone: +84 943 302 809</p>")
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
-})
+});

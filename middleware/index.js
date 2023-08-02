9223c016cc068c8b6e54c3f7f0892be79c6eda14
quagh03 <03.quanghuy.le@gmail.com>
2023-08-02 10:19:27 +0700

    logging

--- middleware/index.js
+++ middleware/index.js
@@ -1,17 +1,22 @@
 import express from "express";
-import { dirname } from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
+import morgan from "morgan";
+import fs from "fs";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+    morgan('combined',fs.createWriteStream(path.join(__dirname, 'access.log'), {flage: 'a'})
+));
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/submit", (req,res) => {
     console.log(req.body);

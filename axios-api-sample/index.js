614df3d32aad3e88d02e852792c46d013d08b7a1
HuceHuyLQ <131230717+HuceHuyLQ@users.noreply.github.com>
2023-08-04 20:29:20 +0700

    add random activity

--- axios-api-sample/index.js
+++ axios-api-sample/index.js
@@ -1,27 +1,28 @@
-import express from "express";
+import epxress from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import path, { dirname } from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const app = express();
+const app = epxress();
 const PORT = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.post("/simple", async (req, res) => {
-    try {
-        const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body["type"]}&participants=${req.body["participants"]}`);
-        res.send(response.data);
-    } catch (error) {
-        console.log("ERROR: " + error.message);
-        res.status(500).send("Error fetching activities.");
+    try{
+        const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body["type"]}&participants${req.body["participants"]}`);
+        var randomNumber = Math.floor(Math.random() * response.data.length + 1);
+        console.log(randomNumber);
+        res.send(response.data[randomNumber]);
+    }catch(err){
+        res.send(err.message);
     }
 });
 
-app.get("/", async (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(PORT, () => {
